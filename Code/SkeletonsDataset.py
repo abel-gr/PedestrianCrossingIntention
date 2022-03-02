a67a3f6e14817bbcd42decff295bd7d562406f49
abel-gr <abel.bugatti@hotmail.com>
2022-03-02 19:34:56 +0100

    Group skeletons to create spatial-temporal graphs

--- Code/SkeletonsDataset.py
+++ Code/SkeletonsDataset.py
@@ -6,15 +6,28 @@ import torch
 from torch.utils.data import Dataset
 from torch_geometric.data import Data
 
-class SkeletonsDataset(Dataset):
+from torch_geometric_temporal.signal import StaticGraphTemporalSignal
 
-    def __init__(self, csv_path, numberOfJoints=25, normalization='minmax', norm_precomputed_values=None, target='cross', transform=None):
+class SkeletonsDataset(Dataset):
+    
+    """
+    Allowed values:
+    
+    csv_path: string
+    numberOfJoints: 25 or 18
+    normalization: minmax or standardization
+    norm_precomputed_values: None or tuple of [max, min] or [mean, std] precomputed values
+    target: 'cross' or 'crossing'
+    info: 'spatial' or int representing the number of frames for a spatial-temporal approach.
+    """
+    def __init__(self, csv_path, numberOfJoints=25, normalization='minmax', norm_precomputed_values=None, target='cross', info='spatial', transform=None):
 
         self.data=[]
         self.csv_path = csv_path
         self.normalization = normalization
         self.norm_precomputed_values = norm_precomputed_values
         self.targetName = target
+        self.graphInfo = info
         
         
         if numberOfJoints == 25:
@@ -73,7 +86,8 @@ class SkeletonsDataset(Dataset):
                 ["REye", "REar"],
                 ["Nose", "LEye"],
                 ["LEye", "LEar"],
-            ]            
+            ]
+            
             
         else:
     
@@ -122,6 +136,7 @@ class SkeletonsDataset(Dataset):
 
 
         self.generateEdges()
+        self.generateEdgeWeights()
 
         self.loadCSV()
         self.processData()
@@ -166,6 +181,13 @@ class SkeletonsDataset(Dataset):
             if part == len(self.pose_parts):
                 part = 0
                 rev = True
+                
+                
+    def generateEdgeWeights(self):
+        
+        self.edge_weights = np.ones(self.edgeindex.shape[1]) # All edges weight 1
+        
+        self.edge_weights = torch.tensor(self.edge_weights, dtype=torch.float)
 
 
     def processData(self):
@@ -266,11 +288,79 @@ class SkeletonsDataset(Dataset):
 
         
         edge_index = torch.tensor(self.edgeindex, dtype=torch.long)
+        
+        # Each graph is independent, so only spatial dimension is used:
+        if self.graphInfo == 'spatial':
+        
+            for iy, x in enumerate(x_values):
 
-        for iy, x in enumerate(x_values):
+                """
+                Data class represents a homogeneous graph.
+                Constructor parameters:
+                
+                x: Node feature matrix with shape [num_nodes, num_node_features]
+                y: Ground-truth labels with arbitrary shape, in this case [2].
+                label: Ground-truth labels (positive class: crossing class) with arbitrary shape, in this case [1].
+                edge_index: Graph connectivity in COO format with shape [2, num_edges]
+                num_nodes: Number of nodes of the graph.
+                """
+                data_element = Data(x=x, y=y_values[iy], label=label_values[iy], edge_index=edge_index, num_nodes=x.shape[0])
+                self.data.append(data_element)
+                
+        else: # Group graphs of the same videos to add the temporal dimension, getting spatial-temporal graphs:
+            
+            videoIDs = self.loadedData['video'].tolist()
+            
+            numFrames = self.graphInfo if type(self.graphInfo) is int else 2
+            
+            for i in range(0, x_values.shape[0] - numFrames*2 + 1):
+                
+                targets_i = target[i:i+numFrames*2]
+                crossing_class_i = targets_i[:, 1]
+                                
+                # If the pedestrian is crossing in the numFrames frames of the temporal graph, or in the next numFrames frames
+                if 1 in crossing_class_i:
+                    label_i = 1
+                    y_value_i = [0, 1]
+                else:
+                    label_i = 0
+                    y_value_i = [1, 0]
+                
+                label_i = torch.tensor(label_i, dtype=torch.float)
+                y_value_i = torch.tensor(y_value_i, dtype=torch.float)
+                                
+                                
+                id_video_i = videoIDs[i]
+                
+                x_temp = []
+                
+                for j in range(0, numFrames):
+                    
+                    id_video_j = videoIDs[i+j]
+                    
+                    # Ensure that next frames are from the same video:
+                    if id_video_i == id_video_j:
+                    
+                        x_temp.append(x_values[i+j])
+                        
+                    # next frame is from the next video
+                    else:
+                        
+                        break
+                        
+                if len(x_temp) == numFrames:
+            
+                    """
+                    StaticGraphTemporalSignal works similarly to the Data class, but x is called features, and y is called targets.
+                    Features and targets are lists of arrays, in which each array is from a different moment in time (temporal dimension).
+                    """
+                    #data_element = StaticGraphTemporalSignal(features=x_temp, targets=y_temp, label=label_temp, edge_index=edge_index, edge_weight=self.edge_weights)
 
-            data_element = Data(x=x, y=y_values[iy], label=label_values[iy], edge_index=edge_index, num_nodes=x.shape[0])
-            self.data.append(data_element)
+                    # StaticGraphTemporalSignal is not compatible with DataLoader, so I use Data class here again, but now x is a list of tensors.
+                    data_element = Data(x_temporal=x_temp, y=y_value_i, label=label_i, edge_index=edge_index, num_nodes=x_values[i].shape[0], edge_weight=self.edge_weights)
+
+                    self.data.append(data_element)            
+            
 
 
     def shuffle(self):
