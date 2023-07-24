58e3c3739ea0596f6dc8d5437c9d9b062488af65
Abel <abel.garciar@e-campus.uab.cat>
2023-07-24 16:20:33 +0200

    Update SkeletonsDataset.py

--- Code/SkeletonsDataset.py
+++ Code/SkeletonsDataset.py
@@ -22,18 +22,22 @@ class SkeletonsDataset(Dataset):
     target: 'cross' or 'crossing'
     info: 'spatial' or int representing the number of frames for a spatial-temporal approach.
     """
-    def __init__(self, csv_path, numberOfJoints=25, normalization='minmax', norm_precomputed_values=None, target='cross', info='spatial', remove_undetected=True, transform=None):
+    def __init__(self, csv_path, numberOfJoints=25, normalization='minmax', norm_precomputed_values=None, target='cross',
+                 info='spatial', remove_undetected=True, use_confidence=True, target_unique_frame=True, transform=None):
 
         self.data=[]
         self.csv_path = csv_path
+        self.numberOfJoints = numberOfJoints
         self.normalization = normalization
         self.norm_precomputed_values = norm_precomputed_values
         self.targetName = target
         self.graphInfo = info
         self.remove_undetected = remove_undetected
+        self.use_confidence = use_confidence
+        self.target_unique_frame = target_unique_frame
         
         
-        if numberOfJoints == 25: # JAAD videos + OpenPose
+        if numberOfJoints == 25: # OpenPose BODY_25
             
             self.body_parts = {
                 "Nose": 0,
@@ -152,7 +156,7 @@ class SkeletonsDataset(Dataset):
             ]
                 
             
-        else:
+        else: # AlphaPose COCO 18 keypoints
     
             self.body_parts = {
                 "Nose": 0,
@@ -258,7 +262,13 @@ class SkeletonsDataset(Dataset):
         if self.remove_undetected:
             # Drop all samples in which the skeleton was not detected on the previous pipeline step
             self.loadedData = self.loadedData[self.loadedData['skeleton_detected']==True]
-
+            # self.loadedData = self.loadedData[self.loadedData['occlusion'] != 'full']
+            # self.loadedData.loc[self.loadedData['skeleton_detected']==False, ['skeleton']] = '[' + ('[0.0, 0.0, 0.0], ' * self.numberOfJoints)[0:-2] + ']'
+            # self.loadedData.loc[self.loadedData['occlusion'] == 'full', ['skeleton']] = '[' + ('[0.0, 0.0, 0.0], ' * self.numberOfJoints)[0:-2] + ']'
+        else:
+            self.loadedData['skeleton'] = self.loadedData['skeleton'].replace(np.nan, '[' + ('[0.0, 0.0, 0.0], ' * self.numberOfJoints)[0:-2] + ']')
+        
+        
         # Preprocess the target of the network:
         
         crossing = self.loadedData[self.targetName].to_numpy()
@@ -281,6 +291,8 @@ class SkeletonsDataset(Dataset):
         
         skeletonData = self.loadedData['skeleton'].tolist()
         videoIDs = self.loadedData['video'].tolist()
+        pedestriansIDs = self.loadedData['ped_id'].tolist()
+        frameIDs = self.loadedData['frame'].tolist()
 
         y_values = []
         x_values = []
@@ -290,7 +302,7 @@ class SkeletonsDataset(Dataset):
 
         # For each skeleton of the dataset
         for i, skeleton in enumerate(skeletonData):
-            
+                        
             skeleton = skeleton.split('], ')
 
             jointCoords = []
@@ -302,21 +314,33 @@ class SkeletonsDataset(Dataset):
                 firstJoint = False
                         
                 coords = np.fromstring(coords, dtype=np.float32, sep=',')
-
+                
+                if not self.use_confidence:
+                    coords = coords[0:2]
+                
                 jointCoords.append(coords)
 
 
             lastJoint = np.fromstring(skeleton[-1][1:-2], dtype=np.float32, sep=',')
+            
+            if not self.use_confidence:
+                lastJoint = lastJoint[0:2]
+            
             jointCoords.append(lastJoint)
 
             jointCoords = np.asarray(jointCoords)
-            
+                        
             videoID = videoIDs[i]
+            pedestrianID = pedestriansIDs[i]
+            frameID = frameIDs[i]
             if videoID in self.original_skeletons:
-                self.original_skeletons[videoID].append(np.copy(jointCoords))
+                if frameID in self.original_skeletons[videoID]:
+                    self.original_skeletons[videoID][frameID][pedestrianID] = np.copy(jointCoords)
+                else:
+                    self.original_skeletons[videoID][frameID] = {pedestrianID: (np.copy(jointCoords))}
             else:
-                self.original_skeletons[videoID] = [np.copy(jointCoords)]
-            
+                self.original_skeletons[videoID] = {frameID: {pedestrianID: (np.copy(jointCoords))}}
+                            
             if self.normalization == 'minmax':
                 
                 xmax = np.amax(jointCoords, axis=0, keepdims=True)
@@ -361,23 +385,6 @@ class SkeletonsDataset(Dataset):
             self.xmean = xmean
             self.xstd = xstd
             
-        """
-        elif self.normalization == 'minmax':
-            
-            # Min-max normalization:
-            
-            if self.norm_precomputed_values is None:
-                xmax = torch.amax(x_values, dim=[1], keepdim=True)                
-                xmin = torch.amin(x_values, dim=[1], keepdim=True)
-            else:
-                xmax, xmin = self.norm_precomputed_values
-            
-            x_values = (x_values - xmin) / (xmax - xmin)
-            
-            self.xmax = xmax
-            self.xmin = xmin
-        """
-            
         if self.normalization == 'max':
             
             # Max normalization:
@@ -416,115 +423,140 @@ class SkeletonsDataset(Dataset):
                         
             numFrames = self.graphInfo if isinstance(self.graphInfo, int) else 2
             
-            for i in range(numFrames*2 - 1, x_values.shape[0]):
-                
-                targets_i = target[i - numFrames*2 + 1:i + 1]
-                crossing_class_i = targets_i[:, 1]
-                                
-                # If the pedestrian is crossing in the numFrames frames of the temporal graph, or in the previous numFrames frames
-                if 1 in crossing_class_i:
-                    label_i = 1
-                    y_value_i = [0, 1]
-                else:
-                    label_i = 0
-                    y_value_i = [1, 0]
-                
-                label_i = torch.tensor(label_i, dtype=torch.float)
-                y_value_i = torch.tensor(y_value_i, dtype=torch.float)
-                                                
-                currentFrame = i - numFrames*2 + 1
-                                
-                id_video_i = videoIDs[currentFrame]
-                                
-                x_temp = []
-                
-                for j in range(0, numFrames):
-                    
-                    id_video_j = videoIDs[currentFrame+j]
-                                        
-                    # Ensure that all frames are from the same video:
-                    if id_video_i == id_video_j:
-                    
-                        x_temp.append(x_values[currentFrame+j])
-                        
-                    # Frame is from another video:
-                    else:
-                        
-                        break
-                        
-                if len(x_temp) == numFrames:
+            unique_pedestrianIDs = pd.unique(self.loadedData['ped_id']).tolist()
             
-                    """
-                    StaticGraphTemporalSignal works similarly to the Data class, but x is called features, and y is called targets.
-                    Features and targets are lists of arrays, in which each array is from a different moment in time (temporal dimension).
-                    """
-                    #data_element = StaticGraphTemporalSignal(features=x_temp, targets=y_temp, label=label_temp, edge_index=edge_index, edge_weight=self.edge_weights)
+            for pedID in unique_pedestrianIDs:
+                                                
+                y_values_ped = []
+                x_values_ped = []
+                
+                id_video_i = -1
+                
+                # For each skeleton of the dataset
+                for j, _ in enumerate(skeletonData):
+                    
+                    videoID_j = videoIDs[j]
+                    
+                    pedestrianID_j = pedestriansIDs[j]
+                                            
+                    if pedID != pedestrianID_j:
+                        continue
+                        
+                    y_values_ped.append(y_values[j].numpy())
+                    x_values_ped.append(x_values[j])
+                    
+                    id_video_i = videoID_j
+                        
+                
+                y_values_ped = np.array(y_values_ped)
+                #x_values_ped = torch.stack(x_values_ped)
+                                
+                for i in range(numFrames - 1, len(x_values_ped)):
 
-                    # StaticGraphTemporalSignal is not compatible with DataLoader, so I use Data class here again, but now x is a list of tensors.
-                    data_element = Data(x_temporal=x_temp, y=y_value_i, label=label_i, edge_index=edge_index,
-                                        num_nodes=x_values[currentFrame].shape[0], edge_weight=self.edge_weights, videoID=id_video_i)
+                    targets_i = y_values_ped[i:i + 1] if self.target_unique_frame else y_values_ped[i - numFrames + 1:i + 1]
+                    crossing_class_i = targets_i[:, 1]
 
-                    self.data.append(data_element)            
+                    # If the pedestrian is crossing in the numFrames frames of the temporal graph, or in the previous numFrames frames
+                    if 1 in crossing_class_i:
+                        label_i = 1
+                        y_value_i = [0, 1]
+                    else:
+                        label_i = 0
+                        y_value_i = [1, 0]
+
+                    label_i = torch.tensor(label_i, dtype=torch.float)
+                    y_value_i = torch.tensor(y_value_i, dtype=torch.float)
+                    
+
+                    x_temp = x_values_ped[i - numFrames + 1:i + 1]
+                                                            
+                    if len(x_temp) == numFrames:
+
+                        """
+                        StaticGraphTemporalSignal works similarly to the Data class, but x is called features, and y is called targets.
+                        Features and targets are lists of arrays, in which each array is from a different moment in time (temporal dimension).
+                        """
+                        #data_element = StaticGraphTemporalSignal(features=x_temp, targets=y_temp, label=label_temp, edge_index=edge_index, edge_weight=self.edge_weights)
+
+                        # StaticGraphTemporalSignal is not compatible with DataLoader, so I use Data class here again, but now x is a list of tensors.
+                        data_element = Data(x_temporal=x_temp, y=y_value_i, label=label_i, edge_index=edge_index,
+                                            num_nodes=x_temp[0].shape[0], edge_weight=self.edge_weights, videoID=id_video_i)
+
+                        self.data.append(data_element)           
             
 
 
             
     def showSkeleton(self, videoNum=0, frameNum=0, textSize=14, showLegend=True, frameImage=None, normalizedSkeletons=True, 
-                     title='Skeleton preview in 2D', show=True, prediction=None, groundtruth=None):
+                     title='Skeleton preview in 2D', show=True, predictions=None, groundtruths=None):
         
         parts = list(self.body_parts.keys())
 
         node_coords = {}
 
-        fig = plt.figure(figsize=(10,10)) if show else Figure(figsize=(10,10))
+        fig = plt.figure(figsize=(10,7), dpi=150) if show else Figure(figsize=(10,7), dpi=150)
             
         ax = fig.add_subplot(1, 1, 1)
-
-        if normalizedSkeletons:
-            skeleton = self.data[videoNum].x_temporal[frameNum][:, 0:2].tolist()
-        else:
-            skeleton = self.original_skeletons[videoNum][frameNum][:, 0:2].tolist()
-
-        for e, sk in enumerate(skeleton):    
-            node_coords[parts[e]] = sk
-
-            ax.scatter(sk[0], sk[1], label=parts[e])
-
-        for edge in self.pose_parts:
-            e0 = node_coords[edge[0]]
-            e1 = node_coords[edge[1]]
-
-            ax.plot([e0[0], e1[0]], [e0[1], e1[1]], color='gray')
-
-            
+        
         if showLegend:
             ax.legend(loc='best', prop={'size': 11})
             
         if frameImage is not None:
             ax.imshow(frameImage)
             
-        if prediction is not None:
+        ped_skeletons = {}
+
+        if normalizedSkeletons:
+            ped_skeletons = self.data[videoNum].x_temporal[frameNum][:, 0:2].tolist()
+        else:
+            video_skeletons = self.original_skeletons[videoNum]
             
-            if groundtruth is not None:
+            if frameNum in video_skeletons:
+                ped_skeletons = video_skeletons[frameNum]
                 
-                textColor = 'green' if prediction == groundtruth else 'red'
                 
-            else:
-                
-                textColor = 'white'
-                
-            sk_arr = np.asarray(skeleton)
-            sk_arr = sk_arr[list(set(np.where(sk_arr != [0.0, 0.0])[0]))] # Removes the missing joints
+        for i, skeleton in enumerate(ped_skeletons.values()):
             
-            if sk_arr.shape[0] == 0: # All joints are missing
-                
-                textCoords = [0, 0]
-                
-            else:
-                
-                textCoords = sk_arr[np.argmin(sk_arr[:, 1])]
-                
-            ax.text(textCoords[0] * 0.95, textCoords[1] * 0.95, prediction, color=textColor, fontsize='medium', backgroundcolor='black')
+            skeleton = skeleton[:, 0:2].tolist()
+
+            for e, sk in enumerate(skeleton):    
+                node_coords[parts[e]] = sk
+
+                ax.scatter(sk[0], sk[1], label=parts[e])
+
+            for edge in self.pose_parts:
+                e0 = node_coords[edge[0]]
+                e1 = node_coords[edge[1]]
+
+                ax.plot([e0[0], e1[0]], [e0[1], e1[1]], color='gray')
+
+            prediction = predictions[i]
+            groundtruth = groundtruths[i]
+            
+            if prediction is not None:
+
+                if groundtruth is not None:
+
+                    textColor = 'green' if prediction == groundtruth else 'red'
+                    
+                    prediction = prediction + " \u2714" if prediction == groundtruth else prediction + " \u2716"
+
+                else:
+
+                    textColor = 'white'
+
+                sk_arr = np.asarray(skeleton)
+                sk_arr = sk_arr[list(set(np.where(sk_arr != [0.0, 0.0])[0]))] # Removes the missing joints
+
+                if sk_arr.shape[0] == 0: # All joints are missing
+
+                    textCoords = [0, 0]
+
+                else:
+
+                    textCoords = sk_arr[np.argmin(sk_arr[:, 1])]
+
+                ax.text(textCoords[0] * 0.95, textCoords[1] * 0.95, prediction, color=textColor, fontsize='medium', backgroundcolor='black')
         
         if show:
             plt.xticks(size=textSize)
